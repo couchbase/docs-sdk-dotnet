fa37737c70451368b62ca1b2aedc04fbe97c76ac
Jeffry Morris <jeffrymorris@gmail.com>
2020-08-11 11:58:32 -0700

    WaitUntilReady managing connections (#130)
    * WaitUntilReady managing connections
    
    * tautologous comments
    
    * evergreen API link
    
    Co-authored-by: Jeff Morris <jeffry.morris@couchbase.com>
    Co-authored-by: Richard Smedley <rgsmedley@gmail.com>

---
+++ modules/howtos/examples/ClusterExample.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Threading.Tasks;
+using Couchbase;
+
+namespace sdk_docs_dotnet_examples
+{
+    // tag::waitUntilReady
+    public class ClusterExample
+    {
+        public async Task Main(string[] args)
+        {
+            var cluster = await Cluster.ConnectAsync("couchbase://127.0.0.1", "username", "password");
+            await cluster.WaitUntilReadyAsync(TimeSpan.FromSeconds(10));
+            var bucket = await cluster.BucketAsync("default");
+            var collection = bucket.DefaultCollection();
+            //..
+        }
+    }
+    // end::waitUntilReady
+
+    // tag::waitUntilReadyBucket
+    public class ClusterExample2
+    {
+        public async Task Main(string[] args)
+        {
+            var cluster = await Cluster.ConnectAsync("couchbase://127.0.0.1", "username", "password");
+            var bucket = await cluster.BucketAsync("default");
+            await bucket.WaitUntilReadyAsync(TimeSpan.FromSeconds(10));
+            var collection = bucket.DefaultCollection();
+            //..
+        }
+    }
+    // end::waitUntilReadyBucket
+}
