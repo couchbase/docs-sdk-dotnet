e202ae29533d0237ba79e9c16d58f039f401c31e
Richard Smedley <rgsmedley@gmail.com>
2020-09-01 16:13:56 +0100

    tag fix

--- modules/howtos/examples/Auth.cs
+++ modules/howtos/examples/Auth.cs
@@ -25,7 +25,7 @@ namespace sdk_docs_dotnet_examples
                 // end::basic]
             }
             {
-                // tag:auth[]
+                // tag::auth[]
                 var options = new ClusterOptions().
                     WithX509CertificateFactory(CertificateFactory.GetCertificatesFromStore(
                         new CertificateStoreSearchCriteria
@@ -37,7 +37,7 @@ namespace sdk_docs_dotnet_examples
                         }));
 
                 var cluster = await Cluster.ConnectAsync(options);
-                // end:auth[]
+                // end::auth[]
             }
         }
     }
