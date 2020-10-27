6d34d18b4063fa05e405db33feaf66f1e165a496
Jeff Morris <jeffry.morris@couchbase.com>
2020-10-27 09:11:43 -0700

    Add analytics and search examples

---
+++ modules/devguide/examples/dotnet/Analytics.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Couchbase.Net.DevGuide
+{
+    public class Analytics : ConnectionBase
+    {
+        private class TestRequest
+        {
+            [JsonProperty("greeting")]
+            public string Greeting { get; set; }
+        }
+
+        public override async Task ExecuteAsync()
+        {
+            const string statement = "SELECT \"hello\" as greeting;";
+
+            var result = await Cluster.AnalyticsQueryAsync<TestRequest>(statement).ConfigureAwait(false);
+
+            await foreach (var row in result)
+            {
+                Console.WriteLine(row);
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            await new Analytics().ExecuteAsync().ConfigureAwait(false);
+        }
+    }
+}
