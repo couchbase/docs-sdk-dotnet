a43382a60e2ff65329f64133a785d71148729cf6
Jeffry Morris <jeffrymorris@gmail.com>
2020-11-16 04:36:40 -0800

    NCBC-2386: Non-JSON, transcoders doc for SDK3 (#159)
    Co-authored-by: Jeff Morris <jeffry.morris@couchbase.com>
    
    DOC-5312 / DOC-7598

---
+++ modules/howtos/examples/Couchbase.Examples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Couchbase.Examples
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cluster = await Cluster.ConnectAsync("couchbase://localhost", "Administrator", "password");
+            var bucket = await cluster.BucketAsync("travel-sample");
+            var collection = bucket.DefaultCollection();
+
+            var transcoding = new Transcoding(collection);
+            await transcoding.RawJsonEncode();
+            await transcoding.RawJsonDecode();
+            await transcoding.String();
+            await transcoding.CustomEncode();
+            await transcoding.CustomDecode();
+            await transcoding.MsgPackEncode();
+            await transcoding.MsgPackDecode();
+            Console.WriteLine("Hit Enter to continue.");
+            Console.Read();
+        }
+    }
+}
