1ef0f2fc7a57e86f9fe3df89086bd92fb1d026f7
Hakim Cassimally <hakim.cassimally@couchbase.com>
2021-03-04 18:17:06 +0000

    DOC-7628 analytics examples for collection and scope

--- modules/devguide/examples/dotnet/Analytics.cs
+++ modules/devguide/examples/dotnet/Analytics.cs
@@ -14,17 +14,46 @@ namespace Couchbase.Net.DevGuide
 
         public override async Task ExecuteAsync()
         {
-            const string statement = "SELECT \"hello\" as greeting;";
-
             await ConnectAsync("travel-sample").ConfigureAwait(false);
 
-            var result = await Cluster.AnalyticsQueryAsync<TestRequest>(statement);
-
-            await foreach (var row in result)
             {
-                Console.WriteLine("Result: " + row.Greeting);
+                const string statement = "SELECT \"hello\" as greeting;";
+                var result = await Cluster.AnalyticsQueryAsync<TestRequest>(statement);
+                await foreach (var row in result)
+                {
+                    Console.WriteLine("Result: " + row.Greeting);
+                }
             }
 
+            var cluster = Cluster;
+
+            {
+                Console.WriteLine("\nHandle Collection");
+                // tag::handle-collection[]
+                var result = await cluster.AnalyticsQueryAsync<dynamic>(
+                    "SELECT airportname, country FROM `airports` WHERE country='France' LIMIT 3");
+
+                await foreach (var row in result)
+                {
+                    Console.WriteLine("Result: " + row);
+                }
+                // end::handle-collection[]
+            }
+
+            {
+                Console.WriteLine("\nHandle Scope");
+                // tag::handle-scope[]
+                IBucket bucket = await cluster.BucketAsync("travel-sample").ConfigureAwait(false);
+                var scope = bucket.Scope("inventory");
+                var result = await scope.AnalyticsQueryAsync<dynamic>(
+                    "SELECT airportname, country FROM `airports-collection` WHERE country='France' LIMIT 2");
+                // end::handle-scope[]
+
+                await foreach (var row in result)
+                {
+                    Console.WriteLine("Result: " + row);
+                }
+            }
         }
 
         static async Task Main(string[] args)
