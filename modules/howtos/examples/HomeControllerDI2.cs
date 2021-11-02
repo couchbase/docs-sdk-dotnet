c641c3e532a891a24ae685de3dfc70e2fb0f3510
Jeffry Morris <jeffrymorris@gmail.com>
2021-11-02 01:06:26 -0700

    Add example of injecting IClusterProvider (#219)
    Co-authored-by: Jeff Morris <jeffry.morris@couchbase.com>

---
+++ modules/howtos/examples/HomeControllerDI2.cs
@@ -0,0 +1,31 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace sdk_docs_dotnet_web_examples.Controllers
+{
+    {
+    // #tag::IClusterProvider[]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IClusterProvider _provider;
+
+        public HomeController(ILogger<HomeController> logger, IClusterProvider provider)
+        {
+            _logger = logger;
+            _provider = provider;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var cluster= await _provider.GetClusterAsync();
+            //do some work
+
+            return View();
+        }
+    }
+    // #end::IClusterProvider[]
+    }
+}
+
