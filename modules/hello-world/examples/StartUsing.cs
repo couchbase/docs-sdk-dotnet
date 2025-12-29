91ae9395bfe5483e7630e44fcb158abe86d7d409
Richard Smedley <rgsmedley@gmail.com>
2025-12-29 11:06:18 +0000

    Antora include:: tags
    DOC-11914

--- modules/hello-world/examples/StartUsing.cs
+++ modules/hello-world/examples/StartUsing.cs
@@ -1,26 +1,36 @@
+// #tag::using[]
 using System;
 using System.Threading.Tasks;
 using Couchbase;await ExampleUsing();
+// #end::using[]
 async Task ExampleUsing()
 {
+    // #tag::connect[]
     var cluster = await Cluster.ConnectAsync(
         // Update these credentials for your Local Couchbase instance!
         "couchbase://localhost",
         "Administrator",
         "password");
+    // #end::connect[]
 
+    // #tag::bucket[]
     // get a bucket reference
     var bucket = await cluster.BucketAsync("travel-sample");
+    // #end::bucket[]
 
+    // #tag::collection[]
     // get a user-defined collection reference
     var scope = await bucket.ScopeAsync("tenant_agent_00");
     var collection = await scope.CollectionAsync("users");
+    // #end::collection[]
 
+    // #tag::upsert-get[]
     // Upsert Document
     var upsertResult = await collection.UpsertAsync("my-document-key", new { Name = "Ted", Age = 31 });
     var getResult = await collection.GetAsync("my-document-key");
 
     Console.WriteLine(getResult.ContentAs<dynamic>());
+    // #end::upsert-get[]
 
     // Call the QueryAsync() function on the scope object and store the result.
     var inventoryScope = bucket.Scope("inventory");
