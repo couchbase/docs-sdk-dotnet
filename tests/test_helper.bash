8174837b413aa39ee1c5eb9e10b51c556baffa67
Simon Dew <39966290+simon-dew@users.noreply.github.com>
2026-03-17 11:58:29 +0000

    Revert "Added the preview/HEAD.yml file and the Tip about for FTS Limits Cap"

---
+++ tests/test_helper.bash
@@ -0,0 +1,19 @@
+setup() {
+	load 'node_modules/bats-support/load'
+	load 'node_modules/bats-assert/load'
+
+	HOWTOS_DIR=../modules/howtos/examples
+	CONCEPT_DOCS_DIR=../modules/concept-docs/examples
+	HELLO_WORLD_DIR=../modules/hello-world/examples
+
+	BATS_TEST_RETRIES=3
+}
+
+function runExample() {
+	cd $1
+	run dotnet script $2
+}
+
+diag() {
+	printf ' # %s\n' "$@" >&3
+}
